f17a7a3df5e05fa06a87e1bb77f8d39156cd988f
xaviguasch <xaviguasch@gmail.com>
2021-08-23 16:42:31 +0200

    implemented login functionality

--- script.js
+++ script.js
@@ -79,8 +79,6 @@ const displayMovements = function (movements) {
   })
 }
 
-displayMovements(account1.movements)
-
 const createUsernames = (accs) => {
   accs.forEach((acc) => {
     acc.username = acc.owner
@@ -93,23 +91,23 @@ const createUsernames = (accs) => {
 
 createUsernames(accounts)
 
-const calcDisplaySummary = (movements) => {
-  const incomes = movements.filter((mov) => mov > 0).reduce((acc, mov) => acc + mov, 0)
+const calcDisplaySummary = (acc) => {
+  const incomes = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0)
   labelSumIn.textContent = `${incomes}€`
 
-  const out = movements.filter((mov) => mov < 0).reduce((acc, mov) => acc + mov, 0)
+  const out = acc.movements.filter((mov) => mov < 0).reduce((acc, mov) => acc + mov, 0)
   labelSumOut.textContent = `${Math.abs(out)}€`
 
-  const interest = movements
+  const interest = acc.movements
     .filter((mov) => mov > 0)
-    .map((deposit) => deposit * 0.012)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
     .filter((int) => int > 1)
     .reduce((acc, int) => acc + int, 0)
 
   labelSumInterest.textContent = `${interest}€`
 }
-
-calcDisplaySummary(account1.movements)
 
 const calcDisplayBalance = (movements) => {
   const balance = movements.reduce((acc, mov) => acc + mov, 0)
@@ -117,7 +115,30 @@ const calcDisplayBalance = (movements) => {
   labelBalance.textContent = `${balance}€`
 }
 
-calcDisplayBalance(account1.movements)
+// EVENT HANDLERS
+let currentAccount
+
+btnLogin.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  currentAccount = accounts.find((acc) => acc.username === inputLoginUsername.value)
+
+  if (currentAccount?.pin === Number(currentAccount.pin)) {
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`
+
+    containerApp.style.opacity = 100
+
+    // Clear fields
+    inputLoginUsername.value = ''
+    inputLoginPin.value = ''
+
+    inputLoginPin.blur()
+
+    displayMovements(currentAccount.movements)
+    calcDisplayBalance(currentAccount.movements)
+    calcDisplaySummary(currentAccount)
+  }
+})
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
