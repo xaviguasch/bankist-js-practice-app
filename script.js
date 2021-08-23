98b4230759ac575b94c49e5872a7d39a3b9bcf6f
xaviguasch <xaviguasch@gmail.com>
2021-08-23 22:40:59 +0200

    added sorting implementation of the movement accounts

--- script.js
+++ script.js
@@ -62,10 +62,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount')
 const inputCloseUsername = document.querySelector('.form__input--user')
 const inputClosePin = document.querySelector('.form__input--pin')
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = ''
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const html = `     
@@ -200,9 +202,32 @@ btnClose.addEventListener('click', (e) => {
   inputClosePin.value = ''
 })
 
+let sorted = false
+
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault()
+  displayMovements(currentAccount.movements, !sorted)
+  sorted = !sorted
+})
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
 
 /////////////////////////////////////////////////
+
+// Flat
+const overallBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0)
+
+console.log(overallBalance)
+
+// FlatMap
+const overallBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0)
+
+console.log(overallBalance)
